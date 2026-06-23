6980ef45cafaa4f3fbcd1a4b88a16d0c046010fa
SankVTW <ddeweert2010@gmail.com>
2026-06-23 07:37:48 -0500

    added a volume bar

--- podcast-app/main_screen.gd
+++ podcast-app/main_screen.gd
@@ -2,16 +2,22 @@ extends Node2D
 
 var bed_music
 var podcast
+var bed_volume 
+var podcast_volume
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	
 	print(bed_music)
+	print(bed_volume)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	
+	bed_volume = $BedMusicControls/HScrollBar.value
+	print("be music volume", bed_volume)
 	pass
 
 
@@ -45,7 +51,8 @@ func _on_file_dialog_file_selected(selected_file: String) -> void:
 func _on_play_bed_music_pressed() -> void:
 	$BedMusicControls/HBoxContainer/PlayBedMusic/AudioStreamPlayer.stream = bed_music
 	print(bed_music)
-	$BedMusicControls/HBoxContainer/PlayBedMusic/AudioStreamPlayer.volume_db = -5.0
+	$BedMusicControls/HBoxContainer/PlayBedMusic/AudioStreamPlayer.volume_db = bed_volume
+	print(bed_volume)
 	$BedMusicControls/HBoxContainer/PlayBedMusic/AudioStreamPlayer.playing = true
 
 func _on_pause_bed_music_pressed() -> void:
@@ -82,3 +89,8 @@ func _on_stop_podcast_pressed() -> void:
 		$PodcastControls/HBoxContainer/AudioStreamPlayer.stream_paused = true
 	elif $PodcastControls/HBoxContainer/AudioStreamPlayer.playing == false:
 		$PodcastControls/HBoxContainer/AudioStreamPlayer.stream_paused = false
+
+
+func _on_h_scroll_bar_value_changed(value: float) -> void:
+	bed_volume = $BedMusicControls/HScrollBar.value
+	pass # Replace with function body.
