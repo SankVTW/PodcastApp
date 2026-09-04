37a4e1f69bece54b5af42913a2852db97cf3d002
SankVTW <ddeweert2010@gmail.com>
2026-09-04 07:49:09 -0500

    Progression updates
    Added an updating podcast play time indicatoir and podcast pause now resumes at the pause point

--- podcast-app/main_screen.gd
+++ podcast-app/main_screen.gd
@@ -2,16 +2,17 @@ extends Node2D
 
 var bed_music
 var podcast
-var bed_volume 
-var podcast_volume
+var bed_volume #:float
+var podcast_volume #:float
 var podcast_playtime
-var podcast_progress
+var podcast_progress : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
+	podcast_playtime = 0.0
 	print(bed_music)
 	print(bed_volume)
+	$Timer.autostart
 	pass # Replace with function body.
 
 
@@ -19,7 +20,7 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	update_bed_music_volume()
 	update_podcast_volume()
-	
+	update_podcast_progress()
 	pass
 
 
@@ -34,11 +35,11 @@ func load_podcast_mp3(path):
 	var file = FileAccess.open(path, FileAccess.READ)
 	var sound = AudioStreamMP3.new()
 	sound.data = file.get_buffer(file.get_length())
-	print("File is ",   sound.get_length(), " seconds long")
+	print("Podcast is ",   sound.get_length(), " seconds long")
 	podcast = sound
 	podcast_playtime = podcast.get_length()
-	$PodcastControls/PodcastPlayerInfo/ProgressArea/ProgressTimeLabel.text = str("00:00:00 / ", podcast_playtime )
-	print(" total play time for file is ", podcast_playtime , " seconds")
+	#$PodcastControls/PodcastPlayerInfo/ProgressArea/ProgressTimeLabel.text = str(podcast_progress," / ", podcast_playtime )
+	print(" total play time for" , file , " is ", podcast_playtime/60 , " minutes")
 func _on_load_bed_music_pressed() -> void:
 	$BedMusicControls/HBoxContainer2/FileDialog.visible = true
 	pass
@@ -49,7 +50,7 @@ func _on_file_dialog_file_selected(selected_file: String) -> void:
 	$BedMusicControls/HBoxContainer2/Label.text = selected_file
 	load_bed_music_mp3(selected_file)
 	print("bed music" , selected_file)
-	
+
 	pass # Replace with function body.
 
 func update_bed_music_volume():
@@ -89,6 +90,7 @@ func _on_podcast_file_dialog_file_selected(path: String) -> void:
 
 func _on_play_podcast_pressed() -> void:
 	$PodcastControls/HBoxContainer/AudioStreamPlayer.stream = podcast
+	$PodcastControls/HBoxContainer/AudioStreamPlayer.seek(podcast_progress)
 	$PodcastControls/HBoxContainer/AudioStreamPlayer.playing = true
 	$PodcastControls/HBoxContainer/AudioStreamPlayer.volume_db = podcast_volume
 	
@@ -98,9 +100,15 @@ func _on_play_podcast_pressed() -> void:
 func _on_stop_podcast_pressed() -> void:
 	if $PodcastControls/HBoxContainer/AudioStreamPlayer.playing == true:
 		$PodcastControls/HBoxContainer/AudioStreamPlayer.stream_paused = true
+		#podcast_progress = $PodcastControls/HBoxContainer/AudioStreamPlayer.get_playback_position()
+		print (podcast_progress)
+		$PodcastControls/HBoxContainer/AudioStreamPlayer.volume_db = podcast_volume
 	elif $PodcastControls/HBoxContainer/AudioStreamPlayer.playing == false:
+		$PodcastControls/HBoxContainer/AudioStreamPlayer.seek(podcast_progress)
 		$PodcastControls/HBoxContainer/AudioStreamPlayer.stream_paused = false
-
+		$PodcastControls/HBoxContainer/AudioStreamPlayer.volume_db = podcast_volume
+		
+		
 
 func _on_h_scroll_bar_value_changed(value: float) -> void:
 	update_bed_music_volume()
@@ -111,4 +119,24 @@ func _on_h_scroll_bar_value_changed(value: float) -> void:
 func _on_podcast_volume_slider_value_changed(value: float) -> void:
 	update_podcast_volume()
 	podcast_volume = $PodcastControls/PodcastVolumeSlider.value
+	pass # Replace with function body.
+
+
+func _on_loop_bed_music_toggled(toggled_on: bool) -> void:
+	match $BedMusicControls/HBoxContainer/LoopBedMusic.disabled:
+		false : $BedMusicControlsmatch/HBoxContainer/PlayBedMusic/AudioStreamPlayer.looping = true
+		false : print ("looping bed music on")
+		true : $BedMusicControlsmatch/HBoxContainer/PlayBedMusic/AudioStreamPlayer.looping = true
+		true : print ("looping bed music off")
+	
+	pass # Replace with function body.
+func update_podcast_progress():
+	podcast_progress = $PodcastControls/HBoxContainer/AudioStreamPlayer.get_playback_position()
+	$PodcastControls/PodcastPlayerInfo/ProgressArea/ProgressTimeLabel.text = str(podcast_progress," / ", podcast_playtime )
+	print(podcast_progress," / ", podcast_playtime )
+
+
+func _on_timer_timeout() -> void:
+	print("timer update")
+	update_podcast_progress()
 	pass # Replace with function body.
